075cf5959fb5063498f692600e676dc4764fdcd4
김태건 <vip00112@apparelbase.com>
2019-06-10 18:06:05 +0900

    1. 화면 캡쳐를 위한 API 추가 2. 전체 화면 캡처 기능 추가

--- src/GTCapture/Model/Capture.cs
+++ src/GTCapture/Model/Capture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,11 +32,11 @@ namespace GTCapture
             CaptureMode mode = Setting.GetCaptureMode(modifier, key);
             if (mode == CaptureMode.None) return;
 
+            Image img = null;
             switch (mode)
             {
                 case CaptureMode.FullScreen:
-                    // TODO : 전체 화면 캡쳐
-                    System.Diagnostics.Process.Start("notepad.exe");
+                    img = CaptureWindow(0, 0, 0, 0);
                     break;
                 case CaptureMode.ActiveProcess:
                     // TODO : 활성화 프로세스 화면 캡쳐
@@ -44,6 +45,10 @@ namespace GTCapture
                     // TODO : 선택 영역 캡쳐
                     break;
             }
+            if (img == null) return;
+
+            // TODO : 캡쳐 후 처리 (클립보드, 뷰화면, 파일로저장 등등)
+            img.Save("Capture.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
         }
         #endregion
 
@@ -54,6 +59,50 @@ namespace GTCapture
             {
                 if (dialog.ShowDialog() != DialogResult.OK) return;
             }
+        }
+        #endregion
+
+        #region Private Method
+        public Image CaptureWindow(int x, int y, int width, int height)
+        {
+            Image img = null;
+            IntPtr handle = IntPtr.Zero;
+            IntPtr src = IntPtr.Zero;
+            IntPtr hdcDest = IntPtr.Zero;
+            IntPtr hBitmap = IntPtr.Zero;
+            try
+            {
+                handle = WindowsAPI.GetDesktopWindow();
+                src = WindowsAPI.GetWindowDC(handle);
+                hdcDest = WindowsAPI.CreateCompatibleDC(src);
+
+                if (width == 0 || height == 0)
+                {
+                    var rec = new WinStructRect();
+                    WindowsAPI.GetWindowRect(handle, ref rec);
+                    if (width == 0 ) width = rec.Right - rec.Left;
+                    if (height == 0 ) height = rec.Bottom - rec.Top;
+                }
+                hBitmap = WindowsAPI.CreateCompatibleBitmap(src, width, height);
+
+                IntPtr hOld = WindowsAPI.SelectObject(hdcDest, hBitmap);
+                WindowsAPI.BitBlt(hdcDest, 0, 0, width, height, src, x, y, WindowsAPI.SRCCOPY);
+                WindowsAPI.SelectObject(hdcDest, hOld);
+
+                img = Image.FromHbitmap(hBitmap);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                WindowsAPI.DeleteObject(hBitmap);
+                WindowsAPI.DeleteDC(hdcDest);
+                WindowsAPI.ReleaseDC(handle, src);
+            }
+
+            return img;
         }
         #endregion
     }
--- src/GTCapture/Model/WindowsAPI.cs
+++ src/GTCapture/Model/WindowsAPI.cs
@@ -15,16 +15,15 @@ namespace GTCapture
         public const int WM_SYSKEYDOWN = 0x0104;
         public const int WM_KEYUP = 0x101;
         public const int WM_SYSKEYUP = 0x105;
+        public const int SRCCOPY = 0x00CC0020;
 
-        #region HotKey
+        #region User32
         [DllImport("user32.dll")]
         public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
 
         [DllImport("user32.dll")]
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-        #endregion
 
-        #region Low Level Keyboard Hook
         [DllImport("user32.dll")]
         public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardHook.CallBack lpfn, IntPtr hMod, uint dwThreadId);
 
@@ -35,8 +34,42 @@ namespace GTCapture
         [DllImport("user32.dll")]
         public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref WinStructRect rect);
+        #endregion
+
+        #region Kernel32
         [DllImport("kernel32.dll")]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
+        #endregion
+
+        #region Gdi32
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hObjectSource, int nXSrc, int nYSrc, int dwRop);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteDC(IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
         #endregion
     }
 
