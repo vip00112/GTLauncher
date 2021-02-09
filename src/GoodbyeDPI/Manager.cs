acaf753bf70dee54c56e28433479083e600788d5
김태건 <vip00112@naver.com>
2021-02-09 21:52:40 +0900

    1. GoodbyeDPI 실행 경로 변경

--- src/GoodbyeDPI/Manager.cs
+++ src/GoodbyeDPI/Manager.cs
@@ -5,17 +5,28 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GoodbyeDPI
 {
     public class Manager
     {
-        private const string FileName = "goodbyedpi.exe";
-
+        private string _fileNameExe;
+        private string _fileNameDll;
+        private string _fileNameSys;
         private Process _proc;
 
         #region Properties
         public bool IsStarted { get; private set; }
+        #endregion
+
+        #region Constructor
+        public Manager()
+        {
+            _fileNameExe = Path.Combine(Application.StartupPath, "goodbyedpi.exe");
+            _fileNameDll = Path.Combine(Application.StartupPath, "WinDivert.dll");
+            _fileNameSys = Path.Combine(Application.StartupPath, "WinDivert64.sys");
+        }
         #endregion
 
         #region Public Method
@@ -29,12 +40,12 @@ namespace GoodbyeDPI
                 proc.Kill();
             }
 
-            File.WriteAllBytes(FileName, Properties.Resources.goodbyedpi);
-            File.WriteAllBytes("WinDivert.dll", Properties.Resources.WinDivert);
-            File.WriteAllBytes("WinDivert64.sys", Properties.Resources.WinDivert64);
+            File.WriteAllBytes(_fileNameExe, Properties.Resources.goodbyedpi);
+            File.WriteAllBytes(_fileNameDll, Properties.Resources.WinDivert);
+            File.WriteAllBytes(_fileNameSys, Properties.Resources.WinDivert64);
 
             _proc = new Process();
-            _proc.StartInfo.FileName = FileName;
+            _proc.StartInfo.FileName = _fileNameExe;
             _proc.StartInfo.UseShellExecute = true;
             _proc.StartInfo.Verb = "runas";
             _proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
@@ -49,15 +60,11 @@ namespace GoodbyeDPI
             _proc.Kill();
             _proc.WaitForExit();
 
-            try
-            {
-                File.Delete(FileName);
-                File.Delete("WinDivert.dll");
-                File.Delete("WinDivert64.sys");
-                _proc = null;
-            }
-            catch { }
+            try { File.Delete(_fileNameExe); } catch { }
+            try { File.Delete(_fileNameDll); } catch { }
+            try { File.Delete(_fileNameSys); } catch { }
 
+            _proc = null;
             IsStarted = false;
         }
         #endregion
